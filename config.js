c7540531561705fe3899e96afd14bb821117512b
rmabdelsamad01 <rmabdelsamad01@gmail.com>
2026-05-10 00:19:38 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,4 +4,4 @@ const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
 });
 let sbUser = null;
 let sbProfile = null;
-const RESEND_API_KEY = re_fp3YXoRR_CNtLHg3irU2V85Phj5gmZiuz;
+const RESEND_API_KEY = 're_fp3YXoRR_CNtLHg3irU2V85Phj5gmZiuz';
