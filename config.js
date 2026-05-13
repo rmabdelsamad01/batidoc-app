5d312358d87beab7e238e962d01833737f86090e
rmabdelsamad01 <rmabdelsamad01@gmail.com>
2026-05-13 22:46:33 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,1 @@
-const { createClient } = supabase;
-const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-  auth: { persistSession: true, autoRefreshToken: true }
-});
-let sbUser = null;
-let sbProfile = null;
+re_SCJtTRYP_3vaMgovbWNbJavqXfhJTJV1H
